92653ca776f1f3986b6c260fe8805f949f9b0b91
Surya Dasari <suryadasari0@gmail.com>
2026-03-14 17:29:14 +0530

    changes in auth.js

--- services/frontend/public/js/auth.js
+++ services/frontend/public/js/auth.js
@@ -1,12 +1,16 @@
 const API = "http://localhost:9000";
 
 function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const msg = document.getElementById("msg");
+
   fetch(`${API}/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      username: username.value,
-      password: password.value
+      username: username,
+      password: password
     })
   })
   .then(r => r.text())
@@ -21,12 +25,16 @@ function login() {
 }
 
 function signup() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const msg = document.getElementById("msg");
+
   fetch(`${API}/signup`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      username: username.value,
-      password: password.value
+      username: username,
+      password: password
     })
   })
   .then(r => r.text())
