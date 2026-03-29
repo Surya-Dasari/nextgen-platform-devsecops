2eb4ba056a219a34d8b9e190d114bfa3ae232ece
root <root@LAPTOP-GOCG20P5.localdomain>
2026-03-29 12:46:42 +0000

    all

---
+++ services/apiservice/src/main/java/com/example/apiservice/kafka/UserProducer.java
@@ -0,0 +1,18 @@
+package com.example.apiservice.kafka;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public UserProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendUserEvent(String userJson) {
+        kafkaTemplate.send("user-events", userJson);
+    }
+}
